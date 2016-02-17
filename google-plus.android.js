290151fd178d77ee963565d01c73fda09c961c8e
Ricardo Bocchi <ricardo@mobilemind.com.br>
2016-02-17 15:56:33 -0200

    merge

--- google-plus.android.js
+++ google-plus.android.js
@@ -62,7 +62,7 @@ function logIn() {
 
       _AndroidApplication.onActivityResult = function (requestCode, resultCode, data) {
      
-       _AndroidApplication.onActivityResult = previousResult;
+        _AndroidApplication.onActivityResult = previousResult;
      
         if (requestCode === RC_SIGN_IN && resultCode === android.app.Activity.RESULT_OK) {
             var result = com.google.android.gms.auth.api.Auth.GoogleSignInApi.getSignInResultFromIntent(data);
@@ -76,3 +76,73 @@ function logIn() {
 
 }
 exports.logIn = logIn;
+
+
+exports.share = function(params){
+
+    //contentURL, contentTitle, imageURL, contentDescription
+
+    var activity = _AndroidApplication.foregroundActivity
+    var builder = new com.google.android.gms.plus.PlusShare.Builder(activity)
+    
+    if(params.imageURL){    
+        var imageUri
+
+        if(params.imageURL.substring(0, 'http'.length) == 'http') // remote not work!!
+            imageUri = android.net.Uri.parse(params.imageURL)
+        else // local file
+            imageUri = android.net.Uri.fromFile(new java.io.File(params.imageURL))
+
+        //builder.setType("image/*")
+        builder.setStream(imageUri)        
+
+    }else{
+    }
+    
+    builder.setType("text/plain")
+
+    //if(params.contentDescription)    
+    builder.setText(params.contentDescription + " " + params.contentURL)
+
+
+    //if(params.contentURL)
+    //    builder.setContentUrl(android.net.Uri.parse(params.contentURL))
+
+    var intent = builder.getIntent()
+
+    /*
+    var intent = new com.google.android.gms.plus.PlusShare.Builder(activity)
+            .setText("Hello Android!")
+            .setType("image/png")
+            .setContentDeepLinkId("testID",
+                    "Test Title",
+                    "Test Description",
+                    android.net.Uri.parse("https://developers.google.com/+/images/interactive-post-android.png"))
+            .getIntent()
+    */
+
+    if (intent.resolveActivity(_AndroidApplication.context.getPackageManager()) != null) {
+        var previousResult = _AndroidApplication.onActivityResult;
+        _AndroidApplication.onActivityResult = function (requestCode, resultCode, data) {            
+            _AndroidApplication.onActivityResult = previousResult;
+        }
+        _AndroidApplication.currentContext.startActivityForResult(intent, 0)
+    }else{
+        var browserIntent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("market://details?id=com.google.android.apps.plus"));        
+        _AndroidApplication.currentContext.startActivity(browserIntent);                
+    }
+}
+
+exports.requestUserProfile = function(result, done){
+
+    //console.log("######## google plus login: nome=" + result.getDisplayName() + ", email=" + result.getEmail() + ", id=" + result.getId())
+    //console.log("IdToken="+result.getIdToken())
+
+    var user = {
+      name: result.getDisplayName(),
+      email: result.getEmail(),
+      token: result.getIdToken()
+    }  
+
+    done(user)
+}
